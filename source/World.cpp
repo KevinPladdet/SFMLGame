681b85d26ce06b182a198c3a28b60b5b410b7acc
KevinPladdet <91886057+KevinPladdet@users.noreply.github.com>
2025-02-06 15:33:52 +0100

    PlatformOne goes up and down + PlayerOne collides with it

--- source/World.cpp
+++ source/World.cpp
@@ -3,65 +3,79 @@
 #include <iostream>
 
 World::World(Engine& eng)
-	: engine(eng)
+	: engine(eng),
+	velocityY(5),
+	minY(4.4),
+	maxY(10)
 {
 	// Creating World
 	b2WorldDef worldDef = b2DefaultWorldDef();
 	worldDef.gravity = { 0.0f, 10.0f };
 	worldId = b2CreateWorld(&worldDef);
 
-	// Creating Ground
+	// Creating PlatformOne
 	b2BodyDef groundBodyDef = b2DefaultBodyDef();
-	groundBodyDef.type = b2_staticBody;
-	groundBodyDef.position = { 3.0f, 500.0f / worldScale };
-	groundId = b2CreateBody(worldId, &groundBodyDef);
+	groundBodyDef.type = b2_kinematicBody;
+	groundBodyDef.position = { 4.0f, 400.0f / worldScale };
+	platformOneId = b2CreateBody(worldId, &groundBodyDef);
 	
-	b2Polygon groundBox = b2MakeBox(0.5f, 0.5f);
+	b2Polygon groundBox = b2MakeBox(1.0f, 0.25f);
 	b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-	b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
+	b2CreatePolygonShape(platformOneId, &groundShapeDef, &groundBox);
 
-	// Creating shape/body
+	// Creating PlayerOne
 	b2BodyDef bodyDef = b2DefaultBodyDef();
 	bodyDef.type = b2_dynamicBody;
 	bodyDef.position = { 3.95f, 4.0f };
-	bodyId = b2CreateBody(worldId, &bodyDef);
+	playerOneId = b2CreateBody(worldId, &bodyDef);
 
 	b2Polygon dynamicBox = b2MakeBox(0.5f, 0.5f);
 
 	b2ShapeDef shapeDef = b2DefaultShapeDef();
 	shapeDef.density = 1.0f;
 	shapeDef.friction = 0.3f;
-
-	b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+	b2CreatePolygonShape(playerOneId, &shapeDef, &dynamicBox);
 }
 
 void World::Update()
 {
 	b2World_Step(worldId, timeStep, subStepCount);
+	
+	// Move left platform
+	b2Vec2 groundPosition = b2Body_GetPosition(platformOneId);
+	std::cout << "PlatformOne Position: (" << groundPosition.x << ", " << groundPosition.y << ")\n";
+
+	if (groundPosition.y < minY || groundPosition.y > maxY)
+	{
+		velocityY *= -1;
+	}
+
+	b2Vec2 velocity = { 0.0f, velocityY };
+	b2Body_SetLinearVelocity(platformOneId, velocity);
 }
 
 void World::Render(sf::RenderWindow& window)
 {
 	// Putting a RectangleShape on the shape/body
-	b2Vec2 position = b2Body_GetPosition(bodyId);
-	b2Rot rotation = b2Body_GetRotation(bodyId);
+	b2Vec2 position = b2Body_GetPosition(playerOneId);
+	b2Rot rotation = b2Body_GetRotation(playerOneId);
 
 	float angle = std::atan2(rotation.s, rotation.c) * 180 / 3.14;
-	std::cout << "Angle Radians: " << angle << "\n";
+	//std::cout << "Angle Radians: " << angle << "\n";
 
 	sf::Vector2f shapeSize(1.0f * worldScale, 1.0f * worldScale);
-	shape.setSize(shapeSize);
-	shape.setOrigin(shapeSize / 2.0f);
-	shape.setPosition(sf::Vector2f(position.x * worldScale, position.y * worldScale));
-	shape.setRotation(angle);
-	engine.window.draw(shape);
+	playerOne.setSize(shapeSize);
+	playerOne.setOrigin(shapeSize / 2.0f);
+	playerOne.setPosition(sf::Vector2f(position.x * worldScale, position.y * worldScale));
+	playerOne.setRotation(angle);
+	engine.window.draw(playerOne);
 
 	// Putting a RectangleShape on the ground
-	b2Vec2 groundPosition = b2Body_GetPosition(groundId);
-	sf::Vector2f groundSize(1.0f * worldScale, 1.0f * worldScale);
-	ground.setFillColor(sf::Color(255, 0, 0));
-	ground.setSize(groundSize);
-	ground.setOrigin(groundSize / 2.0f);
-	ground.setPosition(sf::Vector2f(groundPosition.x * worldScale, groundPosition.y * worldScale));
-	engine.window.draw(ground);
+	b2Vec2 groundPosition = b2Body_GetPosition(platformOneId);
+	sf::Vector2f groundSize(2.0f * worldScale, 0.5f * worldScale);
+	platformOne.setFillColor(sf::Color(255, 0, 0));
+	platformOne.setSize(groundSize);
+	platformOne.setOrigin(groundSize / 2.0f);
+	platformOne.setPosition(sf::Vector2f(groundPosition.x * worldScale, groundPosition.y * worldScale));
+	engine.window.draw(platformOne);
 }
--- source/World.h
+++ source/World.h
@@ -14,15 +14,21 @@ private:
 	Engine& engine;
 
 	b2WorldId worldId;
-	b2BodyId groundId;
-	b2BodyId bodyId;
+	b2BodyId platformOneId;
+	b2BodyId playerOneId;
 
 	// Visualising shapes
-	sf::RectangleShape shape;
-	sf::RectangleShape ground;
+	sf::RectangleShape playerOne;
+	sf::RectangleShape platformOne;
 
 	// worldScale is 50 because 50 pixels is 1 meter in Box2D
 	const int worldScale = 50;
 	float timeStep = 1.0f / 150.0f;
 	int subStepCount = 4;
+
+	// Moving Platform
+	float velocityY;
+	float minY;
+	float maxY;
+
 };
